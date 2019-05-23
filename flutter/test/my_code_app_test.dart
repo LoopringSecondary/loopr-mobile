09194c9479756e886880ecee0fd43bc58cd477d3
kenshin <kenshin_6@163.com>
2019-05-23 17:09:26 +0800

    rename com.example.loopr_flutter to org.loopring.loopr_mobile

--- flutter/test/my_code_app_test.dart
+++ flutter/test/my_code_app_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:loopr_flutter/qr_code/qr_code_app.dart';
+import 'package:loopr_mobile/qr_code/qr_code_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
